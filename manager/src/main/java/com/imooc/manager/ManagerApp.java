a92a7b65fc73daaebd0afec3d7deaeda415be989
HelloWorldwangjiacheng <280167437@qq.com>
2020-03-10 23:09:20 +0800

    这个是之前理财项目的maven版本

---
+++ manager/src/main/java/com/imooc/manager/ManagerApp.java
@@ -0,0 +1,16 @@
+package com.imooc.manager;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @Author w1586
+ * @Date 2020/3/10 22:41
+ * @Cersion 1.0
+ */
+@SpringBootApplication
+public class ManagerApp {
+    public static void main(String[] args){
+        SpringApplication.run(ManagerApp.class, args);
+    }
+}
---
+++ src/main/java/com/imooc/financialdemo/FinancialDemoApplication.java
@@ -0,0 +1,13 @@
+package com.imooc.financialdemo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FinancialDemoApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(FinancialDemoApplication.class, args);
+    }
+
+}
